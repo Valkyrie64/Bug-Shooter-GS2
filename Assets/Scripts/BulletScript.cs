b1ce3bc766465e5a1e799b464e00679a54977766
Valkyrie64 <131360873+Valkyrie64@users.noreply.github.com>
2026-02-13 16:38:32 +0000

    tag update

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -24,7 +24,7 @@ public class BulletScript : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy"))
+        if (other.CompareTag("FlyingEnemy") || other.CompareTag("StoppedEnemy"))
         {
             other.gameObject.SetActive(false);
             Destroy(gameObject);
